0eb4632d8e5b2e5a350102ac2fa83b3e61c5bda0
Jasper Bok <hello@jasperbok.nl>
2024-10-21 13:10:50 +0200

    Use the new error type in the `UsersUserAPIResponse`

--- users_user_resource.go
+++ users_user_resource.go
@@ -8,11 +8,11 @@ import (
 
 // UsersUserAPIResponse represents a response from the /users/user resource.
 //
-// The actual API response also contains the 'errors' and 'debug' fields, but
-// I've never seen them with any other value than `null`, so I cannot define
-// them here yet.
+// The actual API response also contains the 'debug' fields, but I've never seen
+// it with any other value than `null`, so I cannot define it here yet.
 type UsersUserAPIResponse struct {
-	Data Person `json:"data"`
+	Data   Person               `json:"data"`
+	Errors []SimplicateAPIError `json:"errors"`
 }
 
 func (c *UsersClient) User() (Person, error) {
@@ -32,5 +32,10 @@ func (c *UsersClient) User() (Person, error) {
 	}
 
 	err = json.Unmarshal(body, &apiResponse)
+
+	if len(apiResponse.Errors) > 0 {
+		return apiResponse.Data, apiResponse.Errors[0]
+	}
+
 	return apiResponse.Data, err
 }
